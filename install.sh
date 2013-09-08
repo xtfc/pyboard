419587ba483448c5d7dfef6bfd09850ece7ee39c
Philip Dexter <philip.dexter@gmail.com>
2013-09-08 16:00:24 -0400

    Create the admins file in the install script

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ echo "creating a grades directory..."
 mkdir grades
 echo "creating a blank assignments file..."
 touch assignments
-echo "creating a blank sections file..."
-touch sections
+echo "creating a blank admins file..."
+touch admins
 echo "creating a default serverconfig.py..."
 cp serverconfig.py.example serverconfig.py
 
